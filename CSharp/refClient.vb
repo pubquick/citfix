76c7f3c338308b839f7d37479fe5e86ad183b42f
Selva <30388020+pubquick@users.noreply.github.com>
2018-08-23 09:20:36 +0530

    Add files via upload

---
+++ CSharp/refClient.vb
@@ -0,0 +1,149 @@
+﻿Imports System.Collections.Concurrent
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Threading.Tasks
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports System.Net
+
+
+Public Class refClient
+	Dim BaseUrl As String = "https://pubquick.co.uk/"
+	Public Class cwebkey
+		Public Property email As String = String.Empty
+	End Class
+	Public Function Parse(orefquery As Query) As QueryResult
+		Try
+			Using wclient As New Net.Http.HttpClient()
+				wclient.BaseAddress = New Uri(BaseUrl)
+				wclient.DefaultRequestHeaders.Accept.Clear()
+				wclient.DefaultRequestHeaders.Accept.Add(New Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"))
+				Dim postTask = wclient.PostAsJsonAsync("api/refparser", orefquery)
+				postTask.Wait()
+				Dim result = postTask.Result
+				If result.IsSuccessStatusCode Then
+					Dim readTask = result.Content.ReadAsStringAsync()
+					readTask.Wait()
+					Dim converters As Newtonsoft.Json.JsonConverter() = {New RefItemJsonConverter()}
+					Dim qresult As QueryResult = Newtonsoft.Json.JsonConvert.DeserializeObject(Of QueryResult)(readTask.Result, New Newtonsoft.Json.JsonSerializerSettings() With {.Converters = converters})
+					If qresult IsNot Nothing Then
+						Return qresult
+					Else
+						Return Nothing
+					End If
+				Else
+					Return Nothing
+				End If
+			End Using
+		Catch ex As Exception
+			'ExceptionHandling(ex)
+		End Try
+		Return Nothing
+	End Function
+End Class
+
+Public Class RefItemJsonConverter
+	Inherits JsonConverter
+	Public Overrides Function CanConvert(ByVal objectType As Type) As Boolean
+		If objectType = GetType(refData.refitems) Then
+			Return True
+		End If
+		Return False
+	End Function
+
+	Public Overrides Function ReadJson(ByVal reader As JsonReader, ByVal objectType As Type, ByVal existingValue As Object, ByVal serializer As JsonSerializer) As Object
+		Dim jo As JObject = JObject.Load(reader)
+
+		If jo("et") IsNot Nothing Then
+			Select Case jo("et").Value(Of String)
+				Case "grp" : Return jo.ToObject(Of refData.group)(serializer)
+				Case "ri" : Return jo.ToObject(Of refData.refitem)(serializer)
+				Case "sp" : Return jo.ToObject(Of refData.space)(serializer)
+				Case Else
+			End Select
+		Else
+			Return jo.ToObject(Of refData.refitem)(serializer)
+		End If
+
+		Return Nothing
+	End Function
+
+	Public Overrides ReadOnly Property CanWrite As Boolean
+		Get
+			Return False
+		End Get
+	End Property
+
+	Public Overrides Sub WriteJson(ByVal writer As JsonWriter, ByVal value As Object, ByVal serializer As JsonSerializer)
+		Throw New NotImplementedException()
+	End Sub
+End Class
+
+
+
+
+Public Class QueryResult
+	Public Property Result As Boolean = False
+	Public Property Remarks As String = String.Empty
+	Public Property timetaken As Integer
+	Public Property Refs As New ConcurrentBag(Of refData)
+End Class
+
+Public Class refData
+	Public refmeta As New crefinfo
+	Public refs As New List(Of refitems)
+
+	Public Property result As String
+	Public Property matchpercent As Integer = 0
+	Public Property id As String = String.Empty
+	Public Property rid As Long = 0
+	Public Property timetaken As Integer = 0
+	Public Property reftype As String = String.Empty
+	Public Class crefinfo
+		Public Property refid As String = String.Empty
+		Public Property doi As String = String.Empty
+		Public Property pmid As String = String.Empty
+		Public Property pmcid As String = String.Empty
+		Public Property pii As String = String.Empty
+		Public Property isbn As String = String.Empty
+		Public Property pissn As String = String.Empty
+		Public Property eissn As String = String.Empty
+	End Class
+
+	Public MustInherit Class refitems
+
+	End Class
+	Public Class group : Inherits refitems
+		Public ReadOnly Property et As String = "grp"
+		Public Property type As String = String.Empty
+		Public refs As New List(Of refitems)
+	End Class
+	Public Class refitem : Inherits refitems
+		Public ReadOnly Property et As String = "ri"
+		Public Property type As String = String.Empty
+		Public Property text As String = String.Empty
+	End Class
+	Public Class space : Inherits refitems
+		Public ReadOnly Property et As String = "sp"
+		Public Property type As String = "space"
+		Public Property text As String = " "
+	End Class
+End Class
+Public Class Query
+	Public Enum epriority
+		low = 1
+		medium = 2
+		high = 3
+	End Enum
+	Public Property webkey As String = String.Empty
+	Public Property priority As epriority = epriority.medium
+	Public Property batchname As String = String.Empty
+	Public Property Demomode As Boolean = False
+	Public Property refs As New List(Of refitem)
+
+	Public Class refitem
+		Public Property id As String = String.Empty
+		Public Property rid As Long = 0
+		Public Property txt As String = String.Empty
+	End Class
+End Class
